6ac7405a05ad5f7794c88c748c50b391955626f6
Vlad Doster <mvdoster@gmail.com>
2020-12-28 11:29:33 -0600

    (maint) apply sh best practices to add-swap.sh
    - add shebang
    - quote to avoid expansion

--- provisioning/common/add-swap.sh
+++ provisioning/common/add-swap.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 #
 # Add swap space, just in case
 #
-dd if=/dev/zero of=/swapfile count=$1 bs=1MiB
+dd if=/dev/zero of=/swapfile count="$1" bs=1MiB
 chmod 600 /swapfile
 mkswap /swapfile
 echo "/swapfile   swap    swap    sw  0   0" >> /etc/fstab
